cd62f7df1abaef2f3590fecbb9597303b28a2e73
Steve Liddle <liddle@byu.edu>
2022-10-18 22:07:49 -0600

    Initial Commit

---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Stephen Liddle on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
